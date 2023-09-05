060cc0cc689e284c355b139aec0573c0c89197a9
platofff <42435266+platofff@users.noreply.github.com>
2023-09-05 19:13:51 +0300

    WIP

--- python/test.py
+++ python/test.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
 from PIL import Image
-import copy
+import random
 
 from genpattern import GPImgAlpha, gp_genpattern
 
-
-def load_image_as_gp_img_alpha(filepath, copies=1):
+def load_and_modify_image(filepath, copies=1, min_scale=2, max_scale=4, max_rotation=360):
     """
-    Load a PNG image and extract its alpha values, then create a GPImgAlpha instance 
-    with the option to load multiple copies.
+    Load a PNG image, randomly upscale and rotate it, then extract its alpha values.
+    Returns a tuple of (GPImgAlpha, PIL.Image) for each copy.
     """
     with Image.open(filepath) as img:
-
         # Ensure the image has an alpha channel
         if img.mode not in ["RGBA", "LA"]:
             raise ValueError("The image at '%s' doesn't have an alpha channel." % filepath)
+        
+        result = []
 
-        width, height = img.size
+        for _ in range(copies):
+            # Randomly upscale and rotate the image
+            scale_factor = random.randint(min_scale, max_scale)
+            rotation_angle = random.uniform(0, max_rotation)
+            
+            scaled_img = img.resize((int(img.width * scale_factor), int(img.height * scale_factor)), resample=Image.Resampling.NEAREST)
+            rotated_img = scaled_img.rotate(rotation_angle, expand=True, resample=Image.Resampling.BILINEAR)
 
-        # Extract the alpha channel
-        if img.mode == "RGBA":
-            alpha_data = img.split()[3]  # Get the alpha band
-        else:  # LA mode
-            alpha_data = img.split()[1]  # Get the alpha band
-
-    data = alpha_data.tobytes()
-    result = []
-    for _ in range(copies):
-        img_alpha = GPImgAlpha(width, height, data[:])
-        result.append(img_alpha)
+            # Extract the alpha channel
+            if rotated_img.mode == "RGBA":
+                alpha_data = rotated_img.split()[3]  # Get the alpha band
+            else:  # LA mode
+                alpha_data = rotated_img.split()[1]  # Get the alpha band
+            
+            data = alpha_data.tobytes()
+            img_alpha = GPImgAlpha(rotated_img.width, rotated_img.height, data[:])
+            result.append((img_alpha, rotated_img))
 
     return result
 
@@ -37,25 +41,23 @@ def main():
     images = ["test/image1.png", "test/image2.png"]
 
     # Create collections
-    collections = [load_image_as_gp_img_alpha(path, copies=40) for path in images]
-    result = gp_genpattern(collections, 129, 128, 64, 10, 16)
+    collections = [load_and_modify_image(path, copies=256) for path in images]
+    alphas = [[item[0] for item in coll] for coll in collections]
+    result = gp_genpattern(alphas, 512, 512, 64, 0, 0)
 
     # Create a blank canvas
-    canvas = Image.new("RGBA", (128, 128), (255, 255, 255, 255))
+    canvas = Image.new("RGBA", (512, 512), (255, 255, 255, 255))
 
     # For each set of coordinates, paste the corresponding image onto the canvas
     for coll_idx, coll in enumerate(result):
-        for coords in coll:
-            print(coords)
+        for idx, coords in enumerate(coll):
+            rotated_img = collections[coll_idx][idx][1]
             for coord in coords:
-                with Image.open(images[coll_idx]) as pil_img:
-                    canvas.paste(pil_img, coord, mask=pil_img)
-
+                canvas.paste(rotated_img, coord, mask=rotated_img)
 
     # Show the canvas
     canvas.show()
-
-    
+    canvas.save('test.png')
 
 if __name__ == "__main__":
     main()
